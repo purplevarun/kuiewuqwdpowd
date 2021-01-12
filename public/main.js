48fdc45fa7293336d2206b3d70b051b37bc1a5af
purplevarun <purplevarun@gmail.com>
2021-01-12 22:58:47 +0530

    mode

--- public/main.js
+++ public/main.js
@@ -229,7 +229,7 @@ $(function() {
   socket.on('login', (data) => {
     connected = true;
     // Display the welcome message
-    var message = "Welcome to Socket.IO Chat – ";
+    var message = "Welcome to VK Chat – ";
     log(message, {
       prepend: true
     });
